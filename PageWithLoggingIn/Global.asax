2ebecad2c4c4ae34237be04dcabdb7eb508949d2
Daniel Rużewicz <d.ruzew@gmail.com>
2018-02-20 18:07:17 +0100

    Add project files.

---
+++ PageWithLoggingIn/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PageWithLoggingIn.MvcApplication" Language="C#" %>
---
+++ PageWithLoggingIn/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PageWithLoggingIn.Startup))]
+namespace PageWithLoggingIn
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
